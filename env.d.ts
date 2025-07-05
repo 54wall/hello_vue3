aeaa1035219d2900516b3691bb9657b85e57756b
weiqiang <weiqiang_1989@126.com>
2025-07-05 18:11:15 +0800

    feat:005.一个简单的效果

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+//不加报错在main.ts中报出找不到模块“./App.vue”或其相应的类型声明ts(2307)。
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const vueComponent: DefineComponent<{}, {}, any>
+  export default vueComponent
+}
---
+++ src/components/Person.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="person">
+    <h2>姓名!!!:{{ name }}</h2>
+    <h2>年龄:{{ age }}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="showTel">查看联系方式</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Person',
+  data() {
+    return {
+      name: '张三',
+      age: 18,
+      tel: 15555551234,
+    }
+  },
+  methods: {
+    changeName() {
+      this.name = 'zhang-san'
+    },
+    changeAge() {
+      this.age += 1
+    },
+    showTel() {
+      alert(this.tel)
+    },
+  },
+}
+</script>
+
+<style>
+.person {
+  background-color: rgb(149, 79, 44);
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 30px;
+}
+button {
+  margin: 0 5px;
+}
+</style>
