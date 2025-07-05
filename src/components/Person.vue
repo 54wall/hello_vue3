e043c07509372468e52cb9402e4c2f90e41dc61d
weiqiang <weiqiang_1989@126.com>
2025-07-05 20:06:35 +0800

    feat:008.setup的返回值

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -12,41 +12,45 @@
 export default {
   name: 'Person',
   beforeCreate() {
-    console.log('beforeCreate函数被调用了',this)
+    console.log('beforeCreate函数被调用了', this)
     // Options API beforeCreate hook
     // 可以在这里进行一些初始化操作
-  }, 
+  },
   setup() {
-    console.log('setup函数被调用了',this)
+    console.log('setup函数被调用了', this)
     // Composition API setup function
     // 可以在这里定义响应式数据和方法
     let name = '张三'
-    let age = 18  
+    let age = 18
     let tel = 15555551234
 
     function changeName() {
       console.log('changeName函数被调用了')
-      name = 'zhang-san'  // 这里的name是一个普通变量，不是响应式的
+      name = 'zhang-san' // 这里的name是一个普通变量，不是响应式的
       console.log('name:', name)
-    } 
+    }
     function changeAge() {
       console.log('changeAge函数被调用了')
-      age += 1    
+      age += 1
       console.log('age:', age)
     }
     function showTel() {
       console.log('showTel函数被调用了')
       alert(tel)
-    } 
-    return {
-      name,
-      age,
-      tel,  
-      changeName,
-      changeAge,
-      showTel,
     }
+    // return {
+    //   name,
+    //   age,
+    //   tel,
+    //   changeName,
+    //   changeAge,
+    //   showTel,
+    // }
 
+    // return  function(){
+    //   return "哈哈"
+    // }
+    return () => '哈哈'
   },
 }
 </script>
